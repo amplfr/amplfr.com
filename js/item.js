684b02bfb67db033dc196df33ed59d1ae9f79177
Matt VanDuyne <payton109@gmail.com>
2022-09-03 01:21:33 -0400

    fleshed out item.js to create ItemElement from Item JSON, plus other cleanup

--- js/item.js
+++ js/item.js
@@ -1,41 +1,84 @@
 class ItemElement extends HTMLDivElement {
+  #type = "item";
   #isBuilt = false;
-  constructor() {
+
+  constructor(src) {
     super(); // Always call super first in constructor
+
+    if (!!src) {
+      if (typeof src == "string")
+        // src is an ID
+        // this.#isBuilt = this.fetch(src)
+        this.fetch(src).then((rv) => {
+          this.#isBuilt = rv;
+        });
+      if (typeof src == "object")
+        // src is an Item object
+        this.#isBuilt = src;
+    }
   }
 
-  /**
-   * connectedCallback() is called when this element is (re-)added to the DOM, but once is enough for each element
-   */
-  connectedCallback() {
-    // no need to build again if already done so
-    if (this.#isBuilt) return;
+  #buildArtwork() {
+    let artwork = this.#isBuilt["albumid"] ?? `item/${this.#isBuilt["id"]}`;
+    if (artwork == null) return;
 
-    const that = this; // in case this is changed
+    const e = document.createElement("div");
+    e.setAttribute("class", "artwork");
+    e.setAttribute("tabindex", 0);
+
+    artwork = `/albumart/${artwork}.jpg`;
+    e.style.backgroundImage = `url(${artwork})`;
+
+    return e;
+  }
+  #buildArtists() {
+    const attributes = ["name", "area", "started", "ended"];
+    const artists = this.#isBuilt["artists"];
+    if (artists == null) return;
+
+    const e = document.createElement("div");
+    e.setAttribute("class", "artists");
+
+    const buildArtistElement = (artist, rootElement) => {
+      // assign dataset from attributes
+      this.#assignDataset(rootElement.dataset, artist, attributes);
+      rootElement.dataset.id = `artist-${artist.id}`;
+      rootElement.textContent = artist.name;
+    };
+
+    if (artists.length > 1)
+      // create a child element for each Artist, populate it, and append it to e
+      artists.forEach((artist) => {
+        const child = document.createElement("span");
+        buildArtistElement(artist, child);
+        e.appendChild(child);
+      });
+    else buildArtistElement(artists[0], e); // populate E with the single artist
+
+    return e;
+  }
+  #build() {
+    if (this.#isBuilt == true) return; // no need to build again if already done so
+
+    // use the attributes from either the passed Item object, or the HTMLElement.dataset
+    const src = this.#isBuilt || this.dataset;
     const useShadow = false; // toggle if resulting elements should go in shadow DOM instead
     let container;
+
     if (useShadow) container = document.createElement("div");
-    else container = that;
-    container.setAttribute("class", "item");
+    else container = this;
+    container.setAttribute("class", this.#type);
 
-    // handle special case artwork
-    const artwork = document.createElement("div");
-    artwork.setAttribute("class", "artwork");
-    artwork.setAttribute("tabindex", 0);
-    const artworkid =
-      that.getAttribute("data-albumid") ??
-      "item/" + that.getAttribute("data-id");
-    const artworkURL = `/albumart/${artworkid}.jpg`;
-    artwork.style.backgroundImage = `url(${artworkURL})`;
-    container.appendChild(artwork);
+    container.dataset.id = src.id;
+    container.appendChild(this.#buildArtwork(src)); // handle special case artwork
+    container.appendChild(this.#buildArtists(src)); // handle special case artists
 
-    // list of child tags to populate
-    const childTags = ["title", "artists", "collection"];
-    childTags.forEach((tag) => {
-      const dataTag = `data-${tag}`;
-      if (that.hasAttribute(dataTag) || that.hasAttribute(tag)) {
+    // list of additional child tags to populate
+    const childTags = ["title", "collection"];
+    childTags.forEach(function (tag, t, arr) {
+      if (!!src[tag]) {
         // Take attribute content and save it to container's dataset, and inside the childElement div
-        const text = that.getAttribute(dataTag) || that.getAttribute(tag);
+        const text = src[tag];
 
         container.dataset[tag] = text;
 
@@ -45,11 +88,11 @@ class ItemElement extends HTMLDivElement {
 
         container.appendChild(e);
       }
-    });
+    }, this);
 
     this.#isBuilt = true; // get here, and there's no need to run it again
     if (!useShadow) return;
-    const shadow = that.attachShadow({ mode: "open" }); // Create a shadow root
+    const shadow = this.attachShadow({ mode: "open" }); // Create a shadow root
 
     // Apply external styles to the shadow dom
     const linkElem = document.createElement("link");
@@ -59,6 +102,20 @@ class ItemElement extends HTMLDivElement {
     // Attach the created elements to the shadow dom
     shadow.appendChild(linkElem);
     shadow.appendChild(container);
+  }
+
+  #assignDataset(datasetObj, obj, keys = null) {
+    keys = keys || Object.keys(obj);
+    keys.forEach((k) => {
+      if (!!obj[k]) datasetObj[k] = obj[k];
+    });
+  }
+
+  /**
+   * connectedCallback() is called when this element is (re-)added to the DOM, but once is enough for each element
+   */
+  async connectedCallback() {
+    await this.#build();
   }
 
   disconnectedCallback() {}
@@ -78,10 +135,13 @@ class ItemElement extends HTMLDivElement {
       throw err;
     }
 
-    const req = await fetch(`/api/${item}.json`);
-    const rv = await req.json();
+    const req = await fetch(`/api/${id}.json`);
+    return await req.json();
+  }
 
-    return buildItem(rv);
+  get amplfrid() {
+    // ID may be found in a few different spots, especially depending on the phase
+    return this?.dataset?.id || this?.shadowRoot?.id || this.#isBuilt?.id;
   }
 }
 
